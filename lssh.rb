c12b1b38c4056d4fec14d6a015bd198247968d2e
James Standbridge <james.standbridge.git@gmail.com>
2024-01-07 16:29:25 +0100

    formula fix

--- lssh.rb
+++ lssh.rb
@@ -1,12 +1,12 @@
 class Lssh < Formula
     desc "SSH Connection Manager"
     homepage "https://github.com/JamesStandbridge/homebrew-lssh"
-    url "URL_DE_VOTRE_SCRIPT"
-    sha256 "SHA256_DE_VOTRE_SCRIPT"
+    url "https://github.com/JamesStandbridge/lssh-command/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "72b753e583ee7f66fa686cbdb17f7219396db6c51e5bc0bae890bcbce7288ea8  lssh-command-1.0.0.tar.gz"
   
     def install
-        bin.install "lssh.sh"
-        bin.install_symlink "#{bin}/lssh.sh" => "lssh"
+        bin.install "lssh.py" 
+        bin.install_symlink "#{bin}/lssh.py" => "lssh"
     end
 end
   
