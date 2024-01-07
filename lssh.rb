49a61e7a95b58fce942ecd86260637921705b00f
James Standbridge <james.standbridge.git@gmail.com>
2024-01-07 16:29:57 +0100

    fix sha256

--- lssh.rb
+++ lssh.rb
@@ -2,7 +2,7 @@ class Lssh < Formula
     desc "SSH Connection Manager"
     homepage "https://github.com/JamesStandbridge/homebrew-lssh"
     url "https://github.com/JamesStandbridge/lssh-command/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "72b753e583ee7f66fa686cbdb17f7219396db6c51e5bc0bae890bcbce7288ea8  lssh-command-1.0.0.tar.gz"
+    sha256 "72b753e583ee7f66fa686cbdb17f7219396db6c51e5bc0bae890bcbce7288ea8"
   
     def install
         bin.install "lssh.py" 
